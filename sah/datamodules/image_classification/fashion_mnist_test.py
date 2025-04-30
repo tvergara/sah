5f6f48525b42b7427f2a1a8653f5b3b1eb3ea642
tvergara <tomvergara@uc.cl>
2025-04-30 14:59:38 -0400

    Remove image classification datamodules and configs.

--- sah/datamodules/image_classification/fashion_mnist_test.py
+++
@@ -1,9 +0,0 @@
-from sah.conftest import setup_with_overrides
-from sah.datamodules.image_classification.fashion_mnist import FashionMNISTDataModule
-from sah.datamodules.image_classification.image_classification_test import (
-    ImageClassificationDataModuleTests,
-)
-
-
-@setup_with_overrides("datamodule=fashion_mnist")
-class TestFashionMNISTDataModule(ImageClassificationDataModuleTests[FashionMNISTDataModule]): ...
--- sah/datamodules/image_classification/mnist_test.py
+++
@@ -1,9 +0,0 @@
-from sah.conftest import setup_with_overrides
-from sah.datamodules.image_classification.image_classification_test import (
-    ImageClassificationDataModuleTests,
-)
-from sah.datamodules.image_classification.mnist import MNISTDataModule
-
-
-@setup_with_overrides("datamodule=mnist")
-class TestMNISTDataModule(ImageClassificationDataModuleTests[MNISTDataModule]): ...
