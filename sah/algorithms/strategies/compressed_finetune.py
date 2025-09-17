26857af202d62374bfd98f6575ee730309796331
tvergara <tomvergara@uc.cl>
2025-09-17 14:58:11 -0400

    feat(): compute compressed batch

--- sah/algorithms/strategies/compressed_finetune.py
+++ sah/algorithms/strategies/compressed_finetune.py
@@ -18,15 +18,7 @@ class CompressedFinetuneStrategy(BaseStrategy):
             pl_module.automatic_optimization = False
 
     def on_train_start(self, pl_module):
-        self._move_perturbation_tensors_to_device(pl_module)
-
-    def _move_perturbation_tensors_to_device(self, pl_module):
-        device = next(pl_module.model.parameters()).device
-        for child in pl_module.model.modules():
-            if isinstance(child, ModifiedLinear):
-                child.weight_perturbations = child.weight_perturbations.to(device)
-                if child.original_bias is not None:
-                    child.bias_perturbations = child.bias_perturbations.to(device)
+        move_perturbation_tensors_to_device(pl_module)
 
     def training_step(self, pl_module, batch, batch_idx):
         steps_since_last_meta_batch = batch_idx % self.compress_batches_every
@@ -38,6 +30,40 @@ class CompressedFinetuneStrategy(BaseStrategy):
         loss = outputs.loss
         pl_module.log("train/loss", loss, on_step=True, on_epoch=False, prog_bar=True)
         return loss
+
+    def compressed_batch(self, pl_module, batch, accumulation_step=0):
+        compile_perturbations_into_weights(pl_module)
+
+        batch_size = batch["input_ids"].size(0)
+        total_loss = 0
+
+        for i in range(batch_size):
+            single_batch = {k: v[i:i+1] for k, v in batch.items()}
+
+            outputs = pl_module.model(**single_batch)
+            loss = outputs.loss
+            total_loss += loss.item()
+
+            original_params = [p for n, p in pl_module.model.named_parameters() if 'original' in n]
+            grads = torch.autograd.grad(loss, original_params, retain_graph=False)
+
+            slot_idx = accumulation_step * pl_module.batch_size + i
+            param_idx = 0
+
+            for module in pl_module.model.modules():
+                if isinstance(module, ModifiedLinear):
+                    module.weight_perturbations[slot_idx] = grads[param_idx].data.to(torch.float16)
+                    param_idx += 1
+
+                    if module.original_bias is not None:
+                        module.bias_perturbations[slot_idx] = grads[param_idx].data.to(torch.float16)
+                        param_idx += 1
+
+        initialize_scale_parameters(pl_module, self.lr)
+
+        avg_loss = total_loss / batch_size
+        pl_module.log("train/loss", avg_loss, on_step=True, on_epoch=False, prog_bar=True)
+        return avg_loss
 
     def configure_optimizers(self, pl_module):
         pass
@@ -71,3 +97,36 @@ def replace_linear_layers(model, batch_size):
             setattr(model, name, linear)
         else:
             replace_linear_layers(module, batch_size)
+
+
+def move_perturbation_tensors_to_device(pl_module):
+    device = next(pl_module.model.parameters()).device
+    for child in pl_module.model.modules():
+        if isinstance(child, ModifiedLinear):
+            child.weight_perturbations = child.weight_perturbations.to(device)
+            if child.original_bias is not None:
+                child.bias_perturbations = child.bias_perturbations.to(device)
+
+
+def compile_perturbations_into_weights(pl_module):
+    for child in pl_module.model.modules():
+        if isinstance(child, ModifiedLinear):
+            with torch.no_grad():
+                weight_update = torch.einsum('i,ijk->jk', child.scale, child.weight_perturbations)
+                child.original_weight.data += weight_update
+
+                if child.original_bias is not None:
+                    bias_update = torch.einsum('i,ij->j', child.scale, child.bias_perturbations)
+                    child.original_bias.data += bias_update
+
+                child.weight_perturbations.zero_()
+                child.scale.data.zero_()
+                if child.original_bias is not None:
+                    child.bias_perturbations.zero_()
+
+
+def initialize_scale_parameters(pl_module, lr):
+    for child in pl_module.model.modules():
+        if isinstance(child, ModifiedLinear):
+            with torch.no_grad():
+                child.scale.data.fill_(-lr)
