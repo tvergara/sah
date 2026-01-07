dac530059ec86ce88de17bda73a89f67c5afa92d
tvergara <tomvergara@uc.cl>
2026-01-07 18:26:36 -0500

    feat(): set job for vulcan

--- slurm/run-experiment.sh
+++ slurm/run-experiment.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#SBATCH --partition=long
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=48G
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH -o $SCRATCH/slurm-logs/slurm-%j.out
 
 # Usage: sbatch --gres=gpu:a100l:1 slurm/run-experiment.sh qwen metamath adam
