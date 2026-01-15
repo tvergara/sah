ace18ed53c6c226e3f21ace0468045812b585e72
tvergara <tomvergara@uc.cl>
2026-01-14 23:00:09 -0500

    fix(): fix random error in tamia

--- slurm/run-single-job.sh
+++ slurm/run-single-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ~/.bashrc
 module load gcc arrow python/3.12.4 cuda/12.6 httpproxy/1.0
-cd $SCRATCH/sah
+cd /project/aip-sreddy/tvergara/sah
 unset PYTHONPATH
 export HYDRA_AUTO_SCHEMA=0
 source .venv/bin/activate
