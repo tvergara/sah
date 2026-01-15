2695949e002ac5fd6aeb3a926dcb2ed6a7ca094b
tvergara <tomvergara@uc.cl>
2026-01-14 23:17:25 -0500

    fix(): tamia bug

--- slurm/run-single-job.sh
+++ slurm/run-single-job.sh
@@ -4,5 +4,6 @@ module load gcc arrow python/3.12.4 cuda/12.6 httpproxy/1.0
 cd /project/aip-sreddy/tvergara/sah
 unset PYTHONPATH
 export HYDRA_AUTO_SCHEMA=0
+export SCRATCH=/project/aip-sreddy/tvergara
 source .venv/bin/activate
 eval "$@"
